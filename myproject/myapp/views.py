faa75b4a6faa197dfc1792548f5dade3e29a87e3
abdulazizalloughani <abdulaziz.alloughani@cba.ku.edu.kw>
2021-11-23 10:06:33 +0300

    1

--- myproject/myapp/views.py
+++ myproject/myapp/views.py
@@ -1,5 +1,14 @@
 from django.shortcuts import render
-
+from django.http import HttpResponse
 # Create your views here.
 def test_view(request):
     print("request:" , request )
+    return HttpResponse("""
+    <HTML>
+    <head></head>
+    <body>
+    hello world <br />
+    this is our first
+    </body>
+    </HTML>
+    """)
